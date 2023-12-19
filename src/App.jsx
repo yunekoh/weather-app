c19caaf1445ed802d03323581d2602efd8053e50
Haris <82910362+yunekoh@users.noreply.github.com>
2023-12-19 23:50:46 +0100

    Feat(init): App initialization

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import WeatherApp from "./components/WeatherApp/WeatherApp";
+// import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        <WeatherApp />
+      </div>
+    </>
+  );
+}
+
+export default App;
